310b669e6d25266b8a30e91f6ad3c82e7171cb27
ahmad <ahmdramdn47@gmail.com>
2026-05-15 18:26:55 +0700

    fix sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://www.delassahomebakery.id",
-    }),
-  ],
-});
+  plugins: [react(), tailwindcss()],
+})
