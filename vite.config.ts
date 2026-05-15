05359f1c3f69fb193e36dcdfe4992a14751eb41f
ahmad <ahmdramdn47@gmail.com>
2026-05-15 17:56:44 +0700

    update sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://www.delassahomebakery.id",
+    }),
+  ],
+});
